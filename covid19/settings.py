385d5d1ffcfe4f7b0c9e6bcfbc681a6e59bb22e1
Thuhaa <josephthuha@gmail.com>
2020-03-26 23:03:55 +0300

    Added Database
    Mapping Covid19
    
    Co-Authored-By: @Mathenge <zoombiegroove@users.noreply.github.com>

--- covid19/settings.py
+++ covid19/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'covid19.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'covid19',
+        'PASSWORD':'mezenga',
+        'HOST':'localhost',
+        'OWNER':'postgres',
+        'PORT':'5432',
     }
 }
 
